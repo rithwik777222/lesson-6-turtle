ae4f4cdb0471e479563f019dc25300fb5d1fd251
rithwik777222 <rithwikk312@gmail.com>
2025-03-26 19:02:59 +0530

    the square is defeated  :(

---
+++ star hexagram.py
@@ -0,0 +1,26 @@
+import turtle  
+
+turtle.Screen().bgcolor("red")
+b = turtle.Turtle()
+
+b.forward(100) 
+
+b.left(120)
+b.forward(100)
+
+b.left(120)
+b.forward(100)
+
+b.penup()
+b.right(150)
+b.forward(50)
+
+b.pendown()
+b.right(90)
+b.forward(100)
+
+b.right(120)
+b.forward(100)
+
+b.right(120)
+b.forward(100)
